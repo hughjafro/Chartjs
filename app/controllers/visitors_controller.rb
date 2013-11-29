8dd5ad01c6f4f3cfb877e4d4594017cf0eb0e0fe
Christopher Tolbert <cmtolbert1@gmail.com>
2013-11-29 12:21:57 -0800

    Using chartjs labels on pie chart working

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,24 @@
 class VisitorsController < ApplicationController
 
+  def index
+  	# @visitors = Visitor.all
+  end
+
+  def show
+  	# @visitor = Visitor.find(params[:id])
+  end
+
   def new
+  	# @visitor = Visitor.new
+  end
+
+  def create
+  	# @visitor = Visitor.new(visitor_params)
+  	# if @visitor.save
+  	# 	redirect_to @visitor
+  	# else
+  	# 	render new
+  	# end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Superapp::Application.routes.draw do
   root :to => 'visitors#new'
+
+  resources :visitors
 end
